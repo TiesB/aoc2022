42f842e4b512e51525c1b3da5996ed3e32422733
Ties Bolding <tiesnld+git@gmail.com>
2022-12-05 21:35:29 +0100

    Add day05

---
+++ src/day05.rs
@@ -0,0 +1,107 @@
+use std::borrow::BorrowMut;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Error;
+use std::path::Path;
+use std::vec;
+
+use itertools::Itertools;
+
+fn parse_lines(lines: Vec<&str>) -> (HashMap<usize, Vec<char>>, Vec<(usize, usize, usize)>) {
+    let lines_chars = lines
+        .iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let num_cols = (lines_chars[0].len() + 1) / 4;
+    let mut cols: HashMap<usize, Vec<char>> = HashMap::new();
+    for i in 0..num_cols {
+        cols.insert(i, vec![]);
+    }
+
+    let mut moves: Vec<(usize, usize, usize)> = vec![];
+
+    let mut setup_done = false;
+    let mut i = 0;
+    while i < lines_chars.len() {
+        let line = &lines_chars[i];
+        if line.len() == 0 || (!setup_done && lines_chars[i + 1].len() == 0) {
+            setup_done = true;
+            i += 1;
+            continue;
+        }
+
+        if !setup_done {
+            for j in 0..num_cols {
+                let index = 1 + j * 4;
+                let char = line[index];
+                if char != ' ' {
+                    cols.get_mut(&j).unwrap().insert(0, char)
+                }
+            }
+        } else {
+            if let Ok((cnt, src, dst)) = scan_fmt!(
+                &line.into_iter().collect::<String>(),
+                "move {d} from {d} to {d}",
+                usize,
+                usize,
+                usize
+            ) {
+                moves.push((cnt, src - 1, dst - 1));
+            }
+        }
+        i += 1;
+    }
+    (cols, moves)
+}
+
+fn solve1<'a>(
+    (cols, moves): &'a mut (HashMap<usize, Vec<char>>, Vec<(usize, usize, usize)>),
+) -> String {
+    for mov in moves {
+        let cnt = mov.0;
+        let src = mov.1;
+        let dst = mov.2;
+        let mut collect = vec![];
+        for _ in 0..cnt {
+            collect.push(cols.get_mut(&src).unwrap().pop().unwrap());
+        }
+        cols.get_mut(&dst).unwrap().append(&mut collect);
+    }
+    cols.keys()
+        .sorted()
+        .map(|k| cols.get(k).unwrap().last().unwrap())
+        .collect()
+}
+
+fn solve2<'a>(
+    (cols, moves): &'a mut (HashMap<usize, Vec<char>>, Vec<(usize, usize, usize)>),
+) -> String {
+    for mov in moves {
+        let cnt = mov.0;
+        let src = mov.1;
+        let dst = mov.2;
+        let mut collect = vec![];
+        for _ in 0..cnt {
+            collect.push(cols.get_mut(&src).unwrap().pop().unwrap());
+        }
+        collect.reverse();
+        cols.get_mut(&dst).unwrap().append(&mut collect);
+    }
+    cols.keys()
+        .sorted()
+        .map(|k| cols.get(k).unwrap().last().unwrap())
+        .collect()
+}
+
+pub fn main() -> Result<(), Error> {
+    let mut file = File::open(Path::new("inputs/day05.txt"))?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+    let lines = input.split('\n').collect::<Vec<&str>>();
+    let data = parse_lines(lines);
+
+    println!("Part 1: {}", solve1(data.clone().borrow_mut()));
+    println!("Part 2: {}", solve2(data.clone().borrow_mut()));
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate scan_fmt;
+
 mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 fn main() {
     println!("Day 1:");
@@ -12,4 +16,6 @@ fn main() {
     day03::main().unwrap();
     println!("Day 4:");
     day04::main().unwrap();
+    println!("Day 5:");
+    day05::main().unwrap();
 }
