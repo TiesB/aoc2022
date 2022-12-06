10a211a3e3a0c714244771598f7780d597abb44a
Ties Bolding <tiesnld+git@gmail.com>
2022-12-06 11:40:57 +0100

    Day06 add "oneliners"

--- src/day06.rs
+++ src/day06.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
 use std::io::Error;
 use std::path::Path;
@@ -23,10 +23,10 @@ fn solve2(line: &Vec<char>) -> usize {
 }
 
 pub fn main() -> Result<(), Error> {
-    let mut file = File::open(Path::new("inputs/day06.txt"))?;
+    let mut file = fs::File::open(Path::new("inputs/day06.txt"))?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
-    let line = input.trim().chars().collect::<Vec<char>>();
+    let line = input.chars().collect::<Vec<char>>();
 
     println!("Starting part 1");
     let s1start = Instant::now();
@@ -38,5 +38,30 @@ pub fn main() -> Result<(), Error> {
     let s2elapsed = s2start.elapsed();
     println!("Part 1: {}. Took: {:.2?}", s1, s1elapsed);
     println!("Part 2: {}. Took: {:.2?}", s2, s2elapsed);
+
+    let s1o_start = Instant::now();
+    let s1o = fs::read_to_string("inputs/day06.txt")?
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(4)
+        .find_position(|w| w.iter().all_unique())
+        .unwrap()
+        .0
+        + 4;
+    let s1o_elapsed = s1o_start.elapsed();
+
+    let s2o_start = Instant::now();
+    let s2o = fs::read_to_string("inputs/day06.txt")?
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(14)
+        .find_position(|w| w.iter().all_unique())
+        .unwrap()
+        .0
+        + 14;
+    let s2o_elapsed = s2o_start.elapsed();
+    println!("Part 1 oneliner: {} Took: {:.2?}", s1o, s1o_elapsed);
+    println!("Part 2 oneliner: {} Took: {:.2?}", s2o, s2o_elapsed);
+
     Ok(())
 }
