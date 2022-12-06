e0961b44e34a530006b4172d36460caba2cc4dbc
Ties Bolding <tiesnld+git@gmail.com>
2022-12-06 11:07:41 +0100

    Day06 cleanup

--- src/day06.rs
+++ src/day06.rs
@@ -5,10 +5,9 @@ use std::io::Error;
 use std::path::Path;
 use std::time::Instant;
 
-fn solve_for_size(line: &str, size: usize) -> usize {
-    let chars = line.chars().collect::<Vec<char>>();
-    for i in size..chars.len() {
-        let marker: HashSet<&char> = HashSet::from_iter(chars[i - size..i].iter());
+fn solve_for_size(line: &Vec<char>, size: usize) -> usize {
+    for i in size..line.len() {
+        let marker: HashSet<&char> = HashSet::from_iter(line[i - size..i].iter());
         if marker.len() == size {
             return i;
         }
@@ -16,11 +15,11 @@ fn solve_for_size(line: &str, size: usize) -> usize {
     0
 }
 
-fn solve1(line: &str) -> usize {
+fn solve1(line: &Vec<char>) -> usize {
     solve_for_size(line, 4)
 }
 
-fn solve2(line: &str) -> usize {
+fn solve2(line: &Vec<char>) -> usize {
     solve_for_size(line, 14)
 }
 
@@ -28,15 +27,15 @@ pub fn main() -> Result<(), Error> {
     let mut file = File::open(Path::new("inputs/day06.txt"))?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
-    let lines = input.trim().split('\n').collect::<Vec<&str>>();
+    let line = input.trim().chars().collect::<Vec<char>>();
 
     println!("Starting part 1");
     let s1start = Instant::now();
-    let s1 = solve1(lines[0]);
+    let s1 = solve1(&line);
     let s1elapsed = s1start.elapsed();
     println!("Starting part 2");
     let s2start = Instant::now();
-    let s2 = solve2(lines[0]);
+    let s2 = solve2(&line);
     let s2elapsed = s2start.elapsed();
     println!("Part 1: {}. Took: {:.2?}", s1, s1elapsed);
     println!("Part 2: {}. Took: {:.2?}", s2, s2elapsed);
